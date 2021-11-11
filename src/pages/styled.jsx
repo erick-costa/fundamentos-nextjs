6b4c9e29f4f3823169cf7a3ba7c7a664d2700f4c
Erick Costa <erickcosta2509@gmail.com>
2021-11-10 22:23:24 -0300

    usando CSS modularizado

---
+++ src/pages/styled.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Styled.module.css'
+
+export default function Styled() {
+    return (
+        <div className={styles.purple}>
+            <h1>Estilo usando CSS Módulos</h1>
+        </div>
+    )
+}
