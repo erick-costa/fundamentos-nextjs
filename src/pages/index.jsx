f7ecda0a2f4c0cb5a441f276438e0245ef53b598
Erick Costa <erickcosta2509@gmail.com>
2021-11-10 22:45:13 -0300

    navegação entre componentes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
     return (
         <div>
-            <h1>Fundamentos de Next.js & React</h1>
-            <h2>Vamos estudar esse framework</h2>
+            <Link href='/styled'>
+                Componente CSS Modular
+            </Link>
         </div>
     )
 }
--- src/pages/styled.jsx
+++ src/pages/styled.jsx
@@ -1,8 +1,10 @@
 import styles from '../styles/Styled.module.css'
+import Link from 'next/link'
 
 export default function Styled() {
     return (
         <div className={styles.purple}>
+            <Link href='/'>Voltar</Link>
             <h1>Estilo usando CSS Módulos</h1>
         </div>
     )
