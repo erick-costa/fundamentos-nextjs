e0e00dcdef487380f9281af9555972fea000de0b
Erick Costa <erickcosta2509@gmail.com>
2021-11-15 17:01:22 -0300

    navegação dinâmica

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,6 +13,7 @@ export default function Home() {
             <Navigator text='Exemplo' destiny='/example'/>
             <Navigator text='JSX' destiny='/jsx' color='crimson'/>
             <Navigator text='Navegação #01' destiny='/navigation' color='green'/>
+            <Navigator text='Navegação #02' destiny='/client/sp/123' color='blue'/>
        </div>
     )
 }
